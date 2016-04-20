0fce0e574cc3202af60453d5fe8d06b2705d7925
Chris Rice <xiong937@gmail.com>
2016-04-20 01:58:18 -0400

    added more map syles to random selection, improved styling based on map style chosen, and refactored some more code

--- js/mapboxMaps.js
+++ js/mapboxMaps.js
@@ -5,20 +5,44 @@ function randMap () {
 
 	var genRand = Math.random();
 
-	if (genRand <= .2) {
-		$(".jumbotronBg").css("background-color", "#DCEDC8");
+	if (genRand <= .1) {
+		$(".jumbotronBg").css("background-color", "#90A4AE");
 		$(".info").css("color", "#263238");
 		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
 		return 'mapbox.streets';
 	}
-	if (genRand <= .4 && genRand > .2) {
-		return 'mapbox.light';
+	if (genRand <= .2 && genRand > .1) {
+		$(".jumbotronBg").css("background-color", "#90A4AE");
+		$(".info").css("color", "#263238");
+		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
+		return 'mapbox.pencil';
 	}
-	if (genRand <= .6 && genRand > .4) {
+	if (genRand <= .3 && genRand > .2) {
 		return 'mapbox.dark';
 	}
-	if (genRand <= .8 && genRand > .6) {
+	if (genRand <= .4 && genRand > .3) {
+		$(".jumbotronBg").css("background-color", "#90A4AE");
+		$(".info").css("color", "#263238");
+		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
 		return 'mapbox.emerald';
+	}
+	if (genRand <= .5 && genRand > .4) {
+		$(".jumbotronBg").css("background-color", "#90A4AE");
+		$(".info").css("color", "#263238");
+		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
+		return 'mapbox.light';
+	}
+	if (genRand <= .6 && genRand > .5) {
+		return 'mapbox.pirates';
+	}
+	if (genRand <= .7 && genRand > .6) {
+		return 'mapbox.wheatpaste';
+	}
+	if (genRand <= .8 && genRand > .7) {
+		$(".jumbotronBg").css("background-color", "#90A4AE");
+		$(".info").css("color", "#263238");
+		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
+		return 'mapbox.comic';
 	}
 	else
 		return 'mapbox.satellite';
