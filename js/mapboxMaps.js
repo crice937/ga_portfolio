ca9cdb72666fe788333fc2926f2c7dde4d37f638
Chris Rice <xiong937@gmail.com>
2016-04-28 20:28:41 -0400

    removed depreciated features

--- js/mapboxMaps.js
+++ js/mapboxMaps.js
@@ -8,13 +8,13 @@ function randMap () {
 	if (genRand <= .1) {
 		$(".jumbotronBg").css("background-color", "#90A4AE");
 		$(".info").css("color", "#263238");
-		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
+		$(".shadow").css("textShadow", "0px 0px 5px #FFF");
 		return 'mapbox.streets';
 	}
 	if (genRand <= .2 && genRand > .1) {
 		$(".jumbotronBg").css("background-color", "#90A4AE");
 		$(".info").css("color", "#263238");
-		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
+		$(".shadow").css("textShadow", "0px 0px 5px #FFF");
 		return 'mapbox.pencil';
 	}
 	if (genRand <= .3 && genRand > .2) {
@@ -23,28 +23,25 @@ function randMap () {
 	if (genRand <= .4 && genRand > .3) {
 		$(".jumbotronBg").css("background-color", "#90A4AE");
 		$(".info").css("color", "#263238");
-		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
+		$(".shadow").css("textShadow", "0px 0px 5px #FFF");
 		return 'mapbox.emerald';
 	}
 	if (genRand <= .5 && genRand > .4) {
 		$(".jumbotronBg").css("background-color", "#90A4AE");
 		$(".info").css("color", "#263238");
-		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
+		$(".shadow").css("textShadow", "0px 0px 5px #FFF");
 		return 'mapbox.light';
 	}
 	if (genRand <= .6 && genRand > .5) {
 		return 'mapbox.pirates';
 	}
 	if (genRand <= .7 && genRand > .6) {
-		$(".jumbotronBg").css("background-color", "#90A4AE");
-		$(".info").css("color", "#263238");
-		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
 		return 'mapbox.wheatpaste';
 	}
 	if (genRand <= .8 && genRand > .7) {
 		$(".jumbotronBg").css("background-color", "#90A4AE");
 		$(".info").css("color", "#263238");
-		$(".shadow").css("textShadow", "0px 0px 1px #FFF");
+		$(".shadow").css("textShadow", "0px 0px 5px #FFF");
 		return 'mapbox.comic';
 	}
 	else
